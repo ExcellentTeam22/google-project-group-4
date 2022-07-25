d193d95208804fa9e4e18bc6ef3188fe6466f556
Nechama Galili <ntgalili@gmail.com>
2022-07-25 13:19:07 +0300

    empty functions

---
+++ data_struct.py
@@ -0,0 +1,41 @@
+import re
+
+
+
+
+
+a_string= "I, go! and see"
+res = re.findall(r'\w+', a_string)
+print(res)
+
+
+
+def enter_to_dictionary():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+def read_file():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+def pass_all_files():
+    pass
